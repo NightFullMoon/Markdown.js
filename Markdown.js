ffe22c35d2136a3160f97233dd7d9fa9bdb59989
yiqun hong <yiqun hong>
2017-07-10 23:05:31 +0800

    add strong style (a part)

--- Markdown.js
+++ Markdown.js
@@ -1,89 +1,89 @@
 function each(arr, callback) {
-        for (var i = 0; i < arr.length; ++i) {
-                if (false === (callback && callback.call(arr[i], i, arr[i]))) {
-                        return;
-                }
-        }
+	for (var i = 0; i < arr.length; ++i) {
+		if (false === (callback && callback.call(arr[i], i, arr[i]))) {
+			return;
+		}
+	}
 }
 
 function Markdown(content) {
-        // 返回处理完成之后的html文本
-        // var that = Markdown;
-        var temp = [];
+	// 返回处理完成之后的html文本
+	// var that = Markdown;
+	var temp = [];
 
-        temp = content.split(/\n\s*?\n/);
+	temp = content.split(/\n\s*?\n/);
 
-        var that = Markdown;
+	var that = Markdown;
 
-        /*      for(var i in that._handles.block){
-                        that._handles.block[i]()
-                }*/
+	/*      for(var i in that._handles.block){
+	                that._handles.block[i]()
+	        }*/
 
-        each(temp, function(i, block) {
-                // console.log(block);
+	each(temp, function(i, block) {
+		// console.log(block);
 
-                // var 
-                var afterTrans = String(block);
+		// var 
+		var afterTrans = String(block);
 
-                // 处理整块
-                each(that._handles.block, function(i, e) {
-                        var isMatch = e.isMatch(block);
-                        if (isMatch) {
-                                // console.log("匹配到:" + e.name);
-                                afterTrans = e.handle(block);
+		// 处理整块
+		each(that._handles.block, function(i, e) {
+			var isMatch = e.isMatch(block);
+			if (isMatch) {
+				// console.log("匹配到:" + e.name);
+				afterTrans = e.handle(block) || "";
 
-                                // console.log(afterTrans);
-                                return;
-                        }
-                });
+				// console.log(afterTrans);
+				return;
+			}
+		});
 
-                // 处理整行
-                each(that._handles.line, function(i, e) {
-                        if (e.isMatch(afterTrans)) {
-                                // console.log("匹配到:" + e.name);
-                                afterTrans = e.handle(afterTrans);
-                                return false;
-                        }
-                });
+		// 处理整行
+		each(that._handles.line, function(i, e) {
+			if (e.isMatch(afterTrans)) {
+				// console.log("匹配到:" + e.name);
+				afterTrans = e.handle(afterTrans);
+				return false;
+			}
+		});
 
-                //处理行内的样式
-                each(that._handles.inline, function(i, e) {
-                        afterTrans = e.handle(afterTrans);
-                });
+		//处理行内的样式
+		each(that._handles.inline, function(i, e) {
+			afterTrans = e.handle(afterTrans);
+		});
 
-                temp[i] = afterTrans;
-        });
+		temp[i] = afterTrans;
+	});
 
-        return temp.join('');
+	return temp.join('');
 };
 
 //添加一种标记的处理方式
 // type,isMatch,handle
-Markdown.addkHandle = function(obj) {
-        Markdown._handles[obj.type].push(obj);
+Markdown.addHandle = function(obj) {
+	Markdown._handles[obj.type].push(obj);
 }
 
 //将输入拆分成行数组
 Markdown.splitToLine = function(content) {
-        return content.split('\n');
+	return content.split('\n');
 }
 
 // 与上面的函数成对匹配
 Markdown.joinToLine = function(arr) {
-        return arr.join('\n');
+	return arr.join('\n');
 }
 
 // 判断str中除了空格，换行之外是否存在字符
 Markdown.hasChar = function(str) {
-        return 0<str.replace(/\s*/g,'').length;
+	return 0 < str.replace(/\s*/g, '').length;
 }
 
 
 // 处理方式
 Markdown._handles = {
-        inline: [],
-        line: [],
-        block: []
+	inline: [],
+	line: [],
+	block: []
 };
 
 
@@ -95,7 +95,7 @@ Markdown._handles = {
 }*/
 
 function wrapByTag(tag, content) {
-        return '<' + tag + '>' + content + '</' + tag + '>';
+	return '<' + tag + '>' + content + '</' + tag + '>';
 }
 /*
 -列表
@@ -108,29 +108,29 @@ todo:如果空格太多会变成引用是什么鬼？
 #标题h
 */
 (function() {
-        return;
-        // console.log(Markdown);
-        // console.log(each);
+	return;
+	// console.log(Markdown);
+	// console.log(each);
 
-        var BEGIN = /^\s*(#{1,6}) /g;
-        var END = /#* *$/g;
+	var BEGIN = /^\s*(#{1,6}) /g;
+	var END = /#* *$/g;
 
-        var isMatch = function(line) {
-                return !!line && !!line.match(BEGIN);
-        }
+	var isMatch = function(line) {
+		return !!line && !!line.match(BEGIN);
+	}
 
-        var handle = function(line) {
-                var level = line.match(BEGIN)[0].replace(BEGIN, '$1').length;
-                var text = line.replace(BEGIN, '').replace(END, '');
-                return wrapByTag('h' + level, text);
-        }
+	var handle = function(line) {
+		var level = line.match(BEGIN)[0].replace(BEGIN, '$1').length;
+		var text = line.replace(BEGIN, '').replace(END, '');
+		return wrapByTag('h' + level, text);
+	}
 
-        Markdown.addkHandle({
-                name: "h",
-                type: 'line',
-                isMatch: isMatch,
-                handle: handle
-        });
+	Markdown.addHandle({
+		name: "h",
+		type: 'line',
+		isMatch: isMatch,
+		handle: handle
+	});
 })();
 
 /*
@@ -144,46 +144,46 @@ This is an H2
 */
 (function() {
 
-        var regx = /^\s*(={1,}|-{1,})\s*$/g;
+	var regx = /^\s*(={1,}|-{1,})\s*$/g;
 
-        function handle(block) {
-                var arr = Markdown.splitToLine(block);
-                each(arr, function(i, element) {
-                        if (0 == i) {
-                                return;
-                        }
+	function handle(block) {
+		var arr = Markdown.splitToLine(block);
+		each(arr, function(i, element) {
+			if (0 == i) {
+				return;
+			}
 
-                        var match = element.match(regx);
-                        if (match && 0!==i && Markdown.hasChar(arr[i - 1])) {
+			var match = element.match(regx);
+			if (match && 0 !== i && Markdown.hasChar(arr[i - 1])) {
 
-                                var tag ="";
-                                if(-1<element.indexOf('=')){
-                                        tag = "h1";
-                                }else{
-                                         tag = "h2";
-                                }
-                                element = '';
-                                arr[i - 1] = wrapByTag(tag, arr[i - 1]);
-                                return;
-                        }
-                });
-                /*fixme:这边有个问题就是说，如果把已经匹配的标识符整行去掉后保留一个空行，会导致join的时候，多出一个空行，后续处理有block划分有误差的可能，
-                但是如果使数组的长度发生变化，因为在还在循环中，会导致错过一行的情况发生*/
-                var result = Markdown.joinToLine(arr);
-                console.log(result);
+				var tag = "";
+				if (-1 < element.indexOf('=')) {
+					tag = "h1";
+				} else {
+					tag = "h2";
+				}
+				element = '';
+				arr[i - 1] = wrapByTag(tag, arr[i - 1]);
+				return;
+			}
+		});
+		/*fixme:这边有个问题就是说，如果把已经匹配的标识符整行去掉后保留一个空行，会导致join的时候，多出一个空行，后续处理有block划分有误差的可能，
+		但是如果使数组的长度发生变化，因为在还在循环中，会导致错过一行的情况发生*/
+		var result = Markdown.joinToLine(arr);
+		console.log(result);
 
-                return result;
-        }
+		return result;
+	}
 
 
-        Markdown.addkHandle({
-                name: "SetextH",
-                type: 'block',
-                isMatch: function() {
-                        return true;
-                },
-                handle: handle
-        });
+	Markdown.addHandle({
+		name: "SetextH",
+		type: 'block',
+		isMatch: function() {
+			return true;
+		},
+		handle: handle
+	});
 })();
 
 
@@ -201,38 +201,38 @@ todo:
 //TODO:2 暂时不考虑嵌套列表
 */
 (function() {
-        var SPLIT = /\n\s*[\*\-\+] /g;
+	var SPLIT = /\n\s*[\*\-\+] /g;
 
 
-        function handle(block) {
-                var arr = ('\n' + block).split(SPLIT);
+	function handle(block) {
+		var arr = ('\n' + block).split(SPLIT);
 
-                // The first one maybe not a li
-                var isFirstLi = '' === arr[0];
+		// The first one maybe not a li
+		var isFirstLi = '' === arr[0];
 
-                // console.log(arr);
-                each(arr, function(index, element) {
-                        if (!isFirstLi && 0 === index) {
-                                return;
-                        }
-                        arr[index] = wrapByTag('li', element);
-                });
+		// console.log(arr);
+		each(arr, function(index, element) {
+			if (!isFirstLi && 0 === index) {
+				return;
+			}
+			arr[index] = wrapByTag('li', element);
+		});
 
-                if (isFirstLi) {
-                        wrapByTag('ul', arr.join(''));
-                }
+		if (isFirstLi) {
+			wrapByTag('ul', arr.join(''));
+		}
 
-                return arr[0] + wrapByTag('ul', arr.slice(1).join(''));
-        }
+		return arr[0] + wrapByTag('ul', arr.slice(1).join(''));
+	}
 
-        Markdown.addkHandle({
-                name: "unorderList",
-                type: 'block',
-                isMatch: function(content) {
-                        return !!content.match(SPLIT);
-                },
-                handle: handle
-        });
+	Markdown.addHandle({
+		name: "unorderList",
+		type: 'block',
+		isMatch: function(content) {
+			return !!content.match(SPLIT);
+		},
+		handle: handle
+	});
 })();
 
 
@@ -244,37 +244,37 @@ todo:
 
 (function() {
 
-        var SPLIT = /\n\s*[0-9]*\. /g;
+	var SPLIT = /\n\s*[0-9]*\. /g;
 
-        function handle(block) {
-                var arr = ('\n' + block).split(SPLIT);
+	function handle(block) {
+		var arr = ('\n' + block).split(SPLIT);
 
-                // The first one maybe not a li
-                var isFirstLi = '' === arr[0];
+		// The first one maybe not a li
+		var isFirstLi = '' === arr[0];
 
-                // console.log(arr);
-                each(arr, function(index, element) {
-                        if (!isFirstLi && 0 === index) {
-                                return;
-                        }
-                        arr[index] = wrapByTag('li', element);
-                });
+		// console.log(arr);
+		each(arr, function(index, element) {
+			if (!isFirstLi && 0 === index) {
+				return;
+			}
+			arr[index] = wrapByTag('li', element);
+		});
 
-                if (isFirstLi) {
-                        wrapByTag('ol', arr.join(''));
-                }
+		if (isFirstLi) {
+			wrapByTag('ol', arr.join(''));
+		}
 
-                return arr[0] + wrapByTag('ol', arr.slice(1).join(''));
-        }
+		return arr[0] + wrapByTag('ol', arr.slice(1).join(''));
+	}
 
-        Markdown.addkHandle({
-                name: "orderList",
-                type: 'block',
-                isMatch: function(content) {
-                        return !!content.match(SPLIT);
-                },
-                handle: handle
-        });
+	Markdown.addHandle({
+		name: "orderList",
+		type: 'block',
+		isMatch: function(content) {
+			return !!content.match(SPLIT);
+		},
+		handle: handle
+	});
 })();
 
 /*
@@ -282,39 +282,39 @@ todo:
 //atom 内是空行+4空格或者2制表，暂时按照一个制表来处理
 */
 (function() {
-        // return;
+	// return;
 
-        var BEGIN = /^    |\t/g;
+	var BEGIN = /^    |\t/g;
 
-        function isMatch(content) {
-                // console.log(content);
-                var arr = Markdown.splitToLine(content);
-                return BEGIN.test(arr[0]);
-        }
+	function isMatch(content) {
+		// console.log(content);
+		var arr = Markdown.splitToLine(content);
+		return BEGIN.test(arr[0]);
+	}
 
-        function handle(block) {
-                var arr = Markdown.splitToLine(block);
-                // var inRange =true;
-                var codeLine = [];
+	function handle(block) {
+		var arr = Markdown.splitToLine(block);
+		// var inRange =true;
+		var codeLine = [];
 
-                each(arr, function(index, element) {
-                        if (element.match(BEGIN)) {
-                                codeLine.push(element.replace(BEGIN, ''));
-                        } else {
-                                arr.splice(0, index);
-                                return false;
-                        }
+		each(arr, function(index, element) {
+			if (element.match(BEGIN)) {
+				codeLine.push(element.replace(BEGIN, ''));
+			} else {
+				arr.splice(0, index);
+				return false;
+			}
 
-                });
-                return wrapByTag('pre', wrapByTag('code', Markdown.joinToLine(codeLine))) + Markdown.joinToLine(arr);
-        }
+		});
+		return wrapByTag('pre', wrapByTag('code', Markdown.joinToLine(codeLine))) + Markdown.joinToLine(arr);
+	}
 
-        Markdown.addkHandle({
-                name: "code",
-                type: 'block',
-                isMatch: isMatch,
-                handle: handle
-        });
+	Markdown.addHandle({
+		name: "code",
+		type: 'block',
+		isMatch: isMatch,
+		handle: handle
+	});
 
 })();
 
@@ -326,18 +326,16 @@ todo:
 
 
 
-
-
-Markdown.addkHandle({
-        name: "hr",
-        type: 'line',
-        isMatch: function(line) {
-                var REG = /[\*\+\-]/g;
-                return "" === line.replace(REG, "").replace(/\s/g, "") && 2 < (line.match(REG) || []).length
-        },
-        handle: function() {
-                return "<hr>"
-        }
+Markdown.addHandle({
+	name: "hr",
+	type: 'line',
+	isMatch: function(line) {
+		var REG = /[\*\+\-]/g;
+		return "" === line.replace(REG, "").replace(/\s/g, "") && 2 < (line.match(REG) || []).length
+	},
+	handle: function() {
+		return "<hr>"
+	}
 });
 // })();
 
@@ -347,87 +345,110 @@ TODO:
 */
 
 (function() {
-        // 名字需要做完全匹配，因为[]可能也是名字的一部分，但是地址不用
-        // 两者都需要做换行匹配
-        function isMatch(block) {
-                return -1 < block.indexOf('](');
-        }
+	// 名字需要做完全匹配，因为[]可能也是名字的一部分，但是地址不用
+	// 两者都需要做换行匹配
+	function isMatch(block) {
+		return -1 < block.indexOf('](');
+	}
 
-        // 返回未处理完的子串
-        function subHandle(block) {
-                var bracketStack = [];
-                var linkText = "";
-                // var match = block.match(/\]\(/g);
-                // if(!match){return;}
-                var _start = -1;
-                var _end = block.indexOf('](');
-                if (-1 === _end) {
-                        return {
-                                handled: block,
-                                unhandle: ''
-                        };
-                }
-                var text = '';
-                for (var i = _end; _start < i; --i) {
-                        switch (block[i]) {
-                                case ']':
-                                        bracketStack.push(']');
-                                        break;
-                                case '[':
-                                        bracketStack.pop();
-                                        if (0 == bracketStack.length) {
+	// 返回未处理完的子串
+	function subHandle(block) {
+		var bracketStack = [];
+		var linkText = "";
+		// var match = block.match(/\]\(/g);
+		// if(!match){return;}
+		var _start = -1;
+		var _end = block.indexOf('](');
+		if (-1 === _end) {
+			return {
+				handled: block,
+				unhandle: ''
+			};
+		}
+		var text = '';
+		for (var i = _end; _start < i; --i) {
+			switch (block[i]) {
+				case ']':
+					bracketStack.push(']');
+					break;
+				case '[':
+					bracketStack.pop();
+					if (0 == bracketStack.length) {
 
-                                                _start = i;
-                                        }
-                                        break;
-                                default:
-                                        text = block[i] + text;
-                                        break;
-                        }
-                }
+						_start = i;
+					}
+					break;
+				default:
+					text = block[i] + text;
+					break;
+			}
+		}
 
-                var subStr = block.slice(_end + 2);
+		var subStr = block.slice(_end + 2);
 
-                var hrefEnd = subStr.indexOf(')');
-                // console.log(block.slice(2+start+hrefEnd));
+		var hrefEnd = subStr.indexOf(')');
+		// console.log(block.slice(2+start+hrefEnd));
 
-                var title = block.slice(_start + 1, _end);
-                var href = subStr.slice(0, hrefEnd);
+		var title = block.slice(_start + 1, _end);
+		var href = subStr.slice(0, hrefEnd);
 
-                return {
-                        handled: block.slice(0, _start) + '<a href="' + href + '">' + title + '</a>',
-                        unhandle: subStr.slice(hrefEnd + 1)
-                }
-        }
+		return {
+			handled: block.slice(0, _start) + '<a href="' + href + '">' + title + '</a>',
+			unhandle: subStr.slice(hrefEnd + 1)
+		}
+	}
 
-        function handle(block) {
-                var resultStr = '';
-                var result = {
-                        unhandle: block
-                }
+	function handle(block) {
+		var resultStr = '';
+		var result = {
+			unhandle: block
+		}
 
-                do {
-                        result = subHandle(result.unhandle);
-                        resultStr = resultStr + result.handled;
-                        // debugger;
-                } while ('' != result.unhandle)
+		do {
+			result = subHandle(result.unhandle);
+			resultStr = resultStr + result.handled;
+			// debugger;
+		} while ('' != result.unhandle)
 
 
-                return resultStr;
+		return resultStr;
 
-        }
+	}
 
-        Markdown.addkHandle({
-                name: "inlineLink",
-                type: 'block',
-                isMatch: isMatch,
-                handle: handle
-        });
+	Markdown.addHandle({
+		name: "inlineLink",
+		type: 'block',
+		isMatch: isMatch,
+		handle: handle
+	});
 })();
 
 /*
 TODO:强调
 */
+
+(function() {
+
+	var regx = /[\*_]([^\*_]*?)[\*_]/g;
+
+	Markdown.addHandle({
+		name: "inlineLink",
+		type: 'block',
+		isMatch: function(line) {
+			return line.match(regx);
+		},
+		handle: function(line) {
+
+
+			var result = line.replace(regx, "<em>$1</em>");
+			// debugger
+			return result;
+		}
+	});
+
+
+})();
+
 
 /*
 TODO:
